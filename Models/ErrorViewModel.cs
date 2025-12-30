a754d05513eea7046a8b167f57b2238835cbb9fb
keroparator <keroparator@proton.me>
2025-12-30 22:29:44 +0300

    dotnet kurulum yapıldı

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace blood_hospital_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
